a22886fecfa7470e24aa5fb52654ff1d0c9380cf
Minh Nguyen <nguyenthanhminh112000@gmail.com>
2024-04-05 15:52:51 +0700

    fix(MTDS-500008): typescript error in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import '@shared/styles/index.scss';
 import { Provider } from 'react-redux';
 import { store } from '@redux/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
